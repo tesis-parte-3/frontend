b7d298b5a2b7c88503bd1f3762917a1780511099
Yuan <JUANDAVIDGONZALEZNUNEZ@GMAIL.COM>
2024-01-30 09:31:23 -0400

    Funcionable el miniperfil de ajustes con localstorage

--- src/components/Userinfo.init/UserInfoIcons.tsx
+++ src/components/Userinfo.init/UserInfoIcons.tsx
@@ -1,8 +1,31 @@
 import { Avatar, Text, Group } from '@mantine/core';
 import { IconPhoneCall, IconAt } from '@tabler/icons-react';
 import classes from './UserInfoIcons.module.css';
+import { useState, useEffect } from 'react';
 
 function UserInfoIcons() {
+
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    useEffect(() => {
+        const storedPhoneNumber = localStorage.getItem('phoneNumber');
+        const storedName = localStorage.getItem('name');
+        const storedEmail = localStorage.getItem('email');
+
+        if (storedPhoneNumber) {
+            setPhoneNumber(storedPhoneNumber);
+        }
+
+        if (storedName) {
+            setName(storedName);
+        }
+
+        if (storedEmail) {
+            setEmail(storedEmail);
+        }
+    }, []);
+
     return (
         <div>
             <Group wrap="nowrap">
@@ -12,23 +35,23 @@ function UserInfoIcons() {
                     radius="md"
                 />
                 <div>
-                    
+
 
                     <Text fz="lg" fw={500} className={classes.name}>
-                        Nombre
+                        {name}
                     </Text>
 
                     <Group wrap="nowrap" gap={10} mt={3}>
                         <IconAt stroke={1.5} size="1rem" className={classes.icon} />
                         <Text fz="xs" c="dimmed">
-                            Email@urbe.edu.ve
+                            {email}
                         </Text>
                     </Group>
 
                     <Group wrap="nowrap" gap={10} mt={5}>
                         <IconPhoneCall stroke={1.5} size="1rem" className={classes.icon} />
                         <Text fz="xs" c="dimmed">
-                            +0 (000) 123 45 67
+                            {phoneNumber}
                         </Text>
                     </Group>
                 </div>
