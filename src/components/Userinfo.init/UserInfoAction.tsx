6e001e4448802be615521c1ec2d7ebbcfaa596c7
Yuan <JUANDAVIDGONZALEZNUNEZ@GMAIL.COM>
2024-02-20 18:27:24 -0400

    Todo el dia chambeando para terminar la pagina principal, los estudios, cositas del navbar y algunos botones ya redirigen.

--- src/components/Userinfo.init/UserInfoAction.tsx
+++ src/components/Userinfo.init/UserInfoAction.tsx
@@ -11,13 +11,7 @@ import classes from './../../components/Navbar/Navbar.module.css';
 import logo from './../../components/Navbar/logoctm.png';
 import { Avatar, Paper } from '@mantine/core';
 import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-
-interface IUser {
-    name: string;
-    email: string;
-}
-
+import { useState } from 'react';
 
 function UserInfoAction() {
     const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] = useDisclosure(false);
